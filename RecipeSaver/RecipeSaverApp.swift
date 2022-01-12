98f80755bf3d1ba80a29edfe1cf4923edb2c3f08
makhmudov0907 <71522647+makhmudov0907@users.noreply.github.com>
2022-01-12 12:48:31 +0500

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Mirshod Makhmudov on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
